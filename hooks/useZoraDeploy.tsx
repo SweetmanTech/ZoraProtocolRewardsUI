d6d639c65f97a98624296c0676d3331321d054a9
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-11 16:04:55 -0300

    I setup first successful tx to validate Zora Protocol Rewards on Goerli testnet.

--- hooks/useZoraDeploy.tsx
+++ hooks/useZoraDeploy.tsx
@@ -1,19 +1,58 @@
 import { Contract } from "ethers"
-import { useNetwork } from "wagmi"
+import { useAccount, useNetwork } from "wagmi"
 import abi from "../lib/abi-ZoraNFTCreatorProxy.json"
 import { useEthersSigner } from "../lib/useEthersSigner"
 import getZoraNFTCreatorProxyAddress from "../lib/getZoraNFTCreatorProxyAddress"
+import handleTxError from "../lib/handleTxError"
 
 const useZoraDeploy = () => {
   const signer = useEthersSigner()
   const { chain } = useNetwork()
+  const { address } = useAccount()
 
   const createEditionWithReferral = async () => {
-    const zoraNFTCreatorProxyAddres = getZoraNFTCreatorProxyAddress(chain?.id)
-    console.log("create contract", zoraNFTCreatorProxyAddres)
-    const contract = new Contract(zoraNFTCreatorProxyAddres, abi, signer)
-    const name = await contract.contractName()
-    console.log("call createEditionWithReferral", name)
+    try {
+      const zoraNFTCreatorProxyAddres = getZoraNFTCreatorProxyAddress(chain?.id)
+      console.log("create contract", zoraNFTCreatorProxyAddres)
+      const contract = new Contract(zoraNFTCreatorProxyAddres, abi, signer)
+      const contractName = await contract.contractName()
+      console.log("call createEditionWithReferral", contractName)
+      const name = ""
+      const symbol = ""
+      const editionSize = 10_000
+      const royaltyBps = 500
+      const fundsRecipient = address
+      const defaultAdmin = address
+      const salesConfig = {
+        publicSalePrice: 0,
+        maxSalePurchasePerAddress: 100,
+        publicSaleStart: 0,
+        publicSaleEnd: "18446744073709551615",
+        presaleStart: 0,
+        presaleEnd: 0,
+        presaleMerkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+      }
+      const description = ""
+      const animationUri = ""
+      const imageUri = ""
+      const createReferral = address
+      const tx = await contract.createEditionWithReferral(
+        name,
+        symbol,
+        editionSize,
+        royaltyBps,
+        fundsRecipient,
+        defaultAdmin,
+        salesConfig,
+        description,
+        animationUri,
+        imageUri,
+        createReferral,
+      )
+      await tx.wait()
+    } catch (err) {
+      handleTxError(err)
+    }
   }
 
   return {
